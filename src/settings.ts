f2d808b190a8911c7e86b870ae85b85ff8140a87
Philipp Daun <post@philippdaun.net>
2026-01-09 11:59:40 +0100

    Avoid returning from settings method
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- src/settings.ts
+++ src/settings.ts
@@ -32,58 +32,64 @@ export class MainSettingTab extends PluginSettingTab {
 
 		new SettingGroup(containerEl)
 			.setHeading('Hide protocol')
-			.addSetting(setting => setting
+			.addSetting((setting) => { setting
 				.setName(this.format('<kbd>https:</kbd>'))
 				.setDesc('Always enabled')
 				.addToggle(toggle => (toggle)
 					.setDisabled(true)
-					.setValue(true)))
+					.setValue(true))
+			})
 
 		new SettingGroup(containerEl)
 			.setHeading('Hide subdomains')
-			.addSetting(setting => setting
+			.addSetting((setting) => { setting
 				.setName(this.format('<kbd>www.</kbd>'))
 				.addToggle(toggle => (toggle)
 					.setValue(this.plugin.settings.stripWwwSubdomain)
 					.onChange(async (value) => {
 						this.plugin.settings.stripWwwSubdomain = value;
 						await this.plugin.saveSettings();
-					})))
-			.addSetting(setting => setting
+					}))
+			})
+			.addSetting((setting) => { setting
 				.setName(this.format('<kbd>www1.</kbd> + <kbd>www2.</kbd>'))
 				.addToggle(text => (text)
 					.setValue(this.plugin.settings.stripWwwPlusSubdomain)
 					.onChange(async (value) => {
 						this.plugin.settings.stripWwwPlusSubdomain = value;
 						await this.plugin.saveSettings();
-					})))
-			.addSetting(setting => setting
+					}))
+			})
+			.addSetting((setting) => { setting
 				.setName(this.format('<kbd>m.</kbd> + <kbd>mobile.</kbd>'))
 				.addToggle(text => (text)
 					.setValue(this.plugin.settings.stripMobileSubdomain)
 					.onChange(async (value) => {
 						this.plugin.settings.stripMobileSubdomain = value;
 						await this.plugin.saveSettings();
-					})))
-			.addSetting(setting => setting
+					}))
+			})
+			.addSetting((setting) => { setting
 				.setName(this.format('<kbd>amp.</kbd> + <kbd>wap.</kbd>'))
 				.addToggle(text => (text)
 					.setValue(this.plugin.settings.stripAmpSubdomain)
 					.onChange(async (value) => {
 						this.plugin.settings.stripAmpSubdomain = value;
 						await this.plugin.saveSettings();
-					})))
+					}))
+			})
 
 		new SettingGroup(containerEl)
 			.setHeading('Properties')
-			.addSetting(setting => setting
+			.addSetting((setting) => { setting
 				.setName(this.format('Format links in note properties'))
 				.addToggle(text => (text)
 					.setValue(this.plugin.settings.formatMetadata)
 					.onChange(async (value) => {
 						this.plugin.settings.formatMetadata = value;
 						await this.plugin.saveSettings();
-					})))
+					}))
+			})
 	}
 
 	format(html: string): DocumentFragment {
